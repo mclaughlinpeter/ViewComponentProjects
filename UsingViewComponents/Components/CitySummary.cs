d0f321e01a5ee892174bbaa6280455f5a4fd5bd1
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2018-05-19 21:55:06 -0400

    Add CitySummary view component class and apply in _Layout view

--- UsingViewComponents/Components/CitySummary.cs
+++ UsingViewComponents/Components/CitySummary.cs
@@ -1,1 +1,21 @@
 using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using UsingViewComponents.Models;
+
+namespace UsingViewComponents.Components
+{
+    public class CitySummary : ViewComponent
+    {
+        private ICityRepository repository;
+
+        public CitySummary(ICityRepository repo)
+        {
+            repository = repo;
+        }
+
+        public string Invoke()
+        {
+            return $"{repository.Cities.Count()} cities, {repository.Cities.Sum(c => c.Population)} people";
+        }
+    }
+}
